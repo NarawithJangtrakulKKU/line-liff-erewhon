c1fc9328bf7a47485a5f1eec5ac7dc63ed49cfce
Narawith Jangtrakul <narawith.j@kkumail.com>
2025-05-26 21:23:33 +0700

    Add: Categories page with api

--- src/app/allcategories/page.tsx
+++ src/app/allcategories/page.tsx
@@ -1,10 +1,16 @@
 import AllCategoriesPage from '@/components/AllCategoriesPage'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 import React from 'react'
 
 export default function page() {
     return (
         <div>
-            <AllCategoriesPage />
+            <Navbar />
+            <div className="pt-16">
+                <AllCategoriesPage />
+            </div>
+            <Footer />
         </div>
     )
 }
