72bf0937ad3f94b829d07777a8c10ef9fdf910bd
Narawith Jangtrakul <narawith.j@kkumail.com>
2025-05-27 17:53:11 +0700

    Add: Cart page

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,14 @@
+import CartPage from '@/components/CartPage'
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div>
+        <Navbar />
+        <CartPage />
+        <Footer />
+    </div>
+  )
+}
